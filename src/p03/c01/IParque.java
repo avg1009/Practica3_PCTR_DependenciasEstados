7b6dbe373d4810720b9ab3b59eebd0a03a3d6d2c
jose <jri1001>
2021-04-14 13:25:12 +0200

    Interfaz_Parque

--- src/p03/c01/IParque.java
+++ src/p03/c01/IParque.java
@@ -1,9 +1,11 @@
-package p03.c01;
+package src.p03.c01;
 
 public interface IParque {
 	
 	public abstract void entrarAlParque(String puerta);
 
-	// TODO
+	public abstract void entrarAlParque(String puerta);
+	public abstract void salirDelParque(String puerta);
+	
 
 }
